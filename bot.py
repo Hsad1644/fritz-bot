deda1a333bd463593dd45fa2bdfab96f34a523dc
Anupam Dash <anupam.dash0505@gmail.com>
2021-05-17 09:56:45 +0530

    bot status update

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ client = commands.Bot(command_prefix='-')
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game('With ur mom'))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('Holden Tudiks'))
     print('bot is ready')
 
 
