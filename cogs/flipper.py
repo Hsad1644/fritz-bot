9d86d52685c1ef8e883ffc87bfee0de8059702fa
Anupam Dash <anupam.dash0505@gmail.com>
2021-05-17 10:38:58 +0530

    semantics

--- cogs/flipper.py
+++ cogs/flipper.py
@@ -1,21 +1,22 @@
+import discord
 from discord.ext import commands
 from googlesearch import search
 import random
 
 
-class GFinder(commands.Cog):
+class Flipper(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     def flipper(self):
         rest = random.random()
-        return 'heads' if rest < 0.5 else 'tails'
+        return 'HEADS' if rest < 0.5 else 'TAILS'
 
     @commands.Cog.listener()
     async def on_ready(self):
         print('coin flipper ready')
 
     @client.command()
-    async def flip(self, ctx, amt: int):
+    async def flip(self, ctx, amt: int = 1):
         for i in range(amt):
-            await ctx.send(flipper())
+            await ctx.send(f'`{flipper()}`')
