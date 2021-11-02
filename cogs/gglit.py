777a5eb6c89d1fc332bd48ff9c6d3896b54ffd47
Anupam Dash <anupam.dash0505@gmail.com>
2021-11-02 12:33:52 +0530

    update

--- cogs/gglit.py
+++ cogs/gglit.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 client = commands.Bot(command_prefix="!")
 
 
-class GTS(commands.cog):
+class GTS(commands.Cog):
     def __init__(self, client):
         self.client = client
 
