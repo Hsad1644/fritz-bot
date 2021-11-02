7ead95ce9814eb99d77ed668df44f0aebaea32cb
Anupam Dash <anupam.dash0505@gmail.com>
2021-11-02 12:27:47 +0530

    created google that shit troll

---
+++ cogs/gglit.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+
+class GTS(client):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('dumbass alert ready')
+
+    @commands.command()
+    async def googl(self, ctx):
+        with open("../assets/gts.gif", 'rb') as f:
+            pic = discord.file(f)
+            await ctx.send(file=pic)
+
+
+def setup(client):
+    client.add_cog(GTS(client))
